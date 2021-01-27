022e0c2776aad2407b812b5769a2bd6fbbf1a8ee
Verenasworld <verena.felizidas@gmail.com>
2021-01-27 10:10:45 +0100

    first layout css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+
+           <div className="head">
+              <div className='logo'></div>
+
+           </div>
+
+           <div className="item">
+               <h3>@mywebprofi</h3>
+               <Button  variant="outlined" >
+                 Mywebprofi
+               </Button><br/>
+             <Button  variant="outlined" >
+                 Phow Solution
+             </Button><br/>
+               <Button  variant="outlined">
+                 Schoolvision
+             </Button><br/>
+               <Button  variant="outlined">
+                 Elisamassage
+             </Button><br/>
+               <Button  variant="outlined">
+                Titan International
+             </Button><br/>
+               <Button  variant="outlined">
+                  Alex Kiesling
+               </Button>
+           </div>
+        </div>
+
   );
 }
 
