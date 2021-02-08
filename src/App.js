ba1f7627f9b63e50688dfd8649ef6dd662980b17
Verenasworld <verena.felizidas@gmail.com>
2021-02-08 11:07:16 +0100

    playing with darkmode js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
 import {Button} from "@material-ui/core";
+import LogoComponent from "./logo/logo.component";
+import Darkmode from 'darkmode-js';
+
+
+const options = {
+    top: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#ffffff', // default: '#fff'
+    backgroundColor: '#2c1b1b',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#41a38e', // default: '#fff'
+    saveInCookies: false, // default: true,
+    // label: '🌓', // default: ''
+    // autoMatchOsTheme: true // default: true
+}
+
+// const darkmode = new Darkmode(options);
+// darkmode.showWidget();
+
+const darkmode =  new Darkmode(options);
+darkmode.toggle();
+console.log(darkmode.isActivated())
+
+
 
 function App() {
+    new Darkmode().showWidget();
+
   return (
       <div className="container">
 
-           <div className="head">
-              <div className='logo'></div>
-
-           </div>
+          <LogoComponent/>
 
            <div className="item">
                <h3>@mywebprofi</h3>
                <Button  variant="outlined" >
-                 Mywebprofi
+                 mywebprofi.net
                </Button><br/>
              <Button  variant="outlined" >
                  Phow Solution
@@ -22,13 +47,13 @@ function App() {
                  Schoolvision
              </Button><br/>
                <Button  variant="outlined">
-                 Elisamassage
+                 Phow Solution
              </Button><br/>
                <Button  variant="outlined">
-                Titan International
+                 Phow Solution
              </Button><br/>
                <Button  variant="outlined">
-                  Alex Kiesling
+                   Phow Solution
                </Button>
            </div>
         </div>
---
+++ src/logo/logo.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LogoComponent = () => {
+    return (
+
+            <div className="head">
+                <div className='logo'></div>
+
+            </div>
+
+    );
+};
+
+export default LogoComponent;
